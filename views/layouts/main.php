15a4c9c46d10e45794ac4f6a902401417c1a9ce6
Arsen <1b2c@mail.ru>
2015-03-04 20:38:00 +0400

    about text

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -149,13 +149,13 @@ AppAsset::register($this);
         <div class="col-md-6" >
             <h1 class="wow fadeInRight animated" data-wow-delay="0.4s">Hi there, I am Arsen, a Web developer and designer...</h1>
             <br>
-            <p class="wow fadeInRight animated" data-wow-delay="0.4s">My age is 25 years and I'm from <a target="_blank" href="https://www.google.com/maps/place/Yerevan,+Armenia/@40.1533904,44.4885671,12z/data=!3m1!4b1!4m2!3m1!1s0x406aa2dab8fc8b5b:0x3d1479ae87da526a">Yerevan, Armenia</a>. I have been worked in 3 private companies for 3+ years
+            <p class="wow fadeInRight animated" data-wow-delay="0.4s">I'm 25, come from <a target="_blank" href="https://www.google.com/maps/place/Yerevan,+Armenia/@40.1533904,44.4885671,12z/data=!3m1!4b1!4m2!3m1!1s0x406aa2dab8fc8b5b:0x3d1479ae87da526a">Yerevan, Armenia</a>. I worked in 3 private companies for 3+ years
                 and recent 6 month switched to freelancing.
                 <br>
-                I have created lots of public and private websites(some shown above) and always looking for more work to
-                learn from. I am using PHP as my primary programming language. You can see my skillset below this text.
+                I have created lots of public and private websites(some shown above) and I am always looking for something new to
+                learn from. I use PHP as my primary programming language. You can see my skillset below.
                 <br>
-                I am a person who is willing to learn and adapts quickly. I am keen to design and security. I believe in
+                I am a kind of person, who is willing to learn and adapts quickly. I am keen to design and security. I believe in
                 the core principles of Open Source and support it. I have a minimalistic approach to design and
                 usability. Simplicity is beauty they say.
             </p>
