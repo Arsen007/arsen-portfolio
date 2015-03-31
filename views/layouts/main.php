51703a361d6b1f2fcef298612a82409b3ce5d560
Arsen <1b2c@mail.ru>
2015-03-31 12:40:34 +0400

    removed extra projects

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -83,20 +83,20 @@ AppAsset::register($this);
                     <?= Html::a('<i class="fa fa-search"></i>', ['/site/get-work-details', 'id' => 'world_hostels'], ['class' => 'fancybox fancybox.ajax']) ?>
                 </div>
             </div>
-            <div class="masonry-item" style="width: 30%;height: 150px">
-                <img src="img/work/logo/backstock.png" alt="">
-
-                <div class="overlay">
-                    <?= Html::a('<i class="fa fa-search"></i>', ['/site/get-work-details', 'id' => 'backstock'], ['class' => 'fancybox fancybox.ajax']) ?>
-                </div>
-            </div>
-            <div class="masonry-item" style="width: 30%">
-                <img src="img/work/logo/activelocation.png" alt="">
-
-                <div class="overlay">
-                    <?= Html::a('<i class="fa fa-search"></i>', ['/site/get-work-details', 'id' => 'activelocation'], ['class' => 'fancybox fancybox.ajax']) ?>
-                </div>
-            </div>
+<!--            <div class="masonry-item" style="width: 30%;height: 150px">-->
+<!--                <img src="img/work/logo/backstock.png" alt="">-->
+<!---->
+<!--                <div class="overlay">-->
+<!--                    --><?//= Html::a('<i class="fa fa-search"></i>', ['/site/get-work-details', 'id' => 'backstock'], ['class' => 'fancybox fancybox.ajax']) ?>
+<!--                </div>-->
+<!--            </div>-->
+<!--            <div class="masonry-item" style="width: 30%">-->
+<!--                <img src="img/work/logo/activelocation.png" alt="">-->
+<!---->
+<!--                <div class="overlay">-->
+<!--                    --><?//= Html::a('<i class="fa fa-search"></i>', ['/site/get-work-details', 'id' => 'activelocation'], ['class' => 'fancybox fancybox.ajax']) ?>
+<!--                </div>-->
+<!--            </div>-->
             <div class="masonry-item" style="width: 30%">
                 <img src="img/work/logo/xcinex.png" alt="">
 
@@ -111,13 +111,13 @@ AppAsset::register($this);
                     <?= Html::a('<i class="fa fa-search"></i>', ['/site/get-work-details', 'id' => 'threestay'], ['class' => 'fancybox fancybox.ajax']) ?>
                 </div>
             </div>
-            <div class="masonry-item" style="width: 30%">
-                <img src="img/work/logo/valettab.jpg" alt="">
-
-                <div class="overlay">
-                    <?= Html::a('<i class="fa fa-search"></i>', ['/site/get-work-details', 'id' => 'valettab'], ['class' => 'fancybox fancybox.ajax']) ?>
-                </div>
-            </div>
+<!--            <div class="masonry-item" style="width: 30%">-->
+<!--                <img src="img/work/logo/valettab.jpg" alt="">-->
+<!---->
+<!--                <div class="overlay">-->
+<!--                    --><?//= Html::a('<i class="fa fa-search"></i>', ['/site/get-work-details', 'id' => 'valettab'], ['class' => 'fancybox fancybox.ajax']) ?>
+<!--                </div>-->
+<!--            </div>-->
             <div class="masonry-item" style="width: 30%">
                 <img src="img/work/logo/familychangeservices.jpg" alt="">
 
@@ -125,13 +125,13 @@ AppAsset::register($this);
                     <?= Html::a('<i class="fa fa-search"></i>', ['/site/get-work-details', 'id' => 'familychangeservices'], ['class' => 'fancybox fancybox.ajax']) ?>
                 </div>
             </div>
-            <div class="masonry-item" style="width: 30%">
-                <img src="img/work/logo/artzlink.jpg" alt="">
-
-                <div class="overlay">
-                    <?= Html::a('<i class="fa fa-search"></i>', ['/site/get-work-details', 'id' => 'artzlink'], ['class' => 'fancybox fancybox.ajax']) ?>
-                </div>
-            </div>
+<!--            <div class="masonry-item" style="width: 30%">-->
+<!--                <img src="img/work/logo/artzlink.jpg" alt="">-->
+<!---->
+<!--                <div class="overlay">-->
+<!--                    --><?//= Html::a('<i class="fa fa-search"></i>', ['/site/get-work-details', 'id' => 'artzlink'], ['class' => 'fancybox fancybox.ajax']) ?>
+<!--                </div>-->
+<!--            </div>-->
 
         </div>
     </div>
